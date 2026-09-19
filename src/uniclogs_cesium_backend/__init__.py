581dcdd688746ffd6721e67d0bc49b291ef2dcd2
dmitri-mcguckin <dmitri@elips.is>
2026-09-18 22:08:08 -0600

    Update api middleware

--- src/uniclogs_cesium_backend/__init__.py
+++ src/uniclogs_cesium_backend/__init__.py
@@ -1,25 +1,11 @@
-from importlib.metadata import description, name, version
+from importlib.metadata import metadata
 
-from .app import uniclogs_cesium_backend as app
-from .data import GroundStation, Satellite
+from .app import GROUND_STATIONS, SATELLITES, uniclogs_cesium_backend
 
-APP_NAME = name(__name__)
-APP_DESCRIPTION = description(__name__)
-APP_VERSION = version(__name__)
+__metadata__ = metadata(__name__)
 
-DEFAULT_HOST = "0.0.0.0"
-DEFAULT_PORT = 9000
-DEFAULT_API_PREFIX = "/"
+APP_NAME: str = __metadata__["name"]
+APP_DESCRIPTION: str = __metadata__["description"]
+APP_VERSION: str = __metadata__["version"]
 
-SATELLITES = [
-    # Satellite('OreSat0', 52017, '2022-026'),
-    Satellite("OreSat0.5", 60525, "2024-149"),
-]
-
-GROUND_STATIONS = [
-    GroundStation("UniClOGS EB", 45.509054, -122.681394, 50, 0),
-]
-
-__all__ = [
-    "app"
-]
+__all__ = ["uniclogs_cesium_backend", "SATELLITES", "GROUND_STATIONS"]
--- src/uniclogs_cesium_backend/app.py
+++ src/uniclogs_cesium_backend/app.py
@@ -5,10 +5,22 @@ from os import getenv
 from flask import Flask
 from flask_cors import CORS
 
-from .data import Data
+from .data import Data, GroundStation, Satellite
 from .views import view_czml, view_groundstation, view_passes, view_satellite, view_tiles
 
+DEFAULT_HOST = "0.0.0.0"
+DEFAULT_PORT = 9000
+DEFAULT_API_PREFIX = "/"
 DEFAULT_DATA_DIR = getenv("DATA_DIR", "../data")
+
+SATELLITES = [
+    # Satellite('OreSat0', 52017, '2022-026'),
+    Satellite("OreSat0.5", 60525, "2024-149"),
+]
+
+GROUND_STATIONS = [
+    GroundStation("UniClOGS EB", 45.509054, -122.681394, 50, 0),
+]
 
 
 class App(Flask):
@@ -20,18 +32,12 @@ class App(Flask):
     def __init__(
         self: App,
         data: Data,
-        host: str = "localhost",
-        port: int = 9000,
-        api_prefix: str = "/",
-        data_dir: str = DEFAULT_DATA_DIR,
+        api_prefix: str = DEFAULT_API_PREFIX,
         debug: bool = False,
     ):
         super().__init__(__name__)
 
         # Setup app parameters
-        self.host = host
-        self.port = port
-        self.data_dir = data_dir
         self.data = data
         self.debug = debug
 
@@ -46,9 +52,16 @@ class App(Flask):
         super().run(host=self.host, port=self.port, debug=self.debug)
 
 
-uniclogs_cesium_backend = App()
+uniclogs_cesium_backend = App(
+    data=Data(
+        satellites=SATELLITES,
+        groundstations=GROUND_STATIONS,
+    )
+)
 
-CORS.CORS_ORIGINS = [
-    "*"
+ALLOWED_ORIGINS: list[str] = [
+    "http://127.0.0.1:3000",
+    "http://:localhost:3000",
+    "https://cesium.uniclogs.org",
 ]
-CORS(uniclogs_cesium_backend)
+CORS(app=uniclogs_cesium_backend, origins=ALLOWED_ORIGINS)
