7aa213969a2c1ad25bd023983b4a370ea855ee76
dimitry-mcguckin <dmitri@elips.is>
2026-09-20 15:18:49 -0600

    Update code checks

--- src/uniclogs_cesium_backend/__init__.py
+++ src/uniclogs_cesium_backend/__init__.py
@@ -2,7 +2,7 @@ from importlib.metadata import metadata
 
 from uniclogs_cesium_backend.app import App
 
-from .app import GROUND_STATIONS, SATELLITES, create
+from .app import GROUND_STATIONS, SATELLITES, build_app
 
 __metadata__ = metadata(__name__)
 
@@ -10,6 +10,6 @@ APP_NAME: str = __metadata__["name"]
 APP_DESCRIPTION: str = __metadata__["description"]
 APP_VERSION: str = __metadata__["version"]
 
-application: App = create()
+application: App = build_app()
 
 __all__ = ["application", "SATELLITES", "GROUND_STATIONS"]
